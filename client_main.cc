3517d8c6b9e45e044b9c46b7d51973faef767e7f
Adam Homolya <adam.homolya@gmail.com>
2018-03-26 18:49:50 +0200

    comment in main

--- client_main.cc
+++ client_main.cc
@@ -4,15 +4,13 @@
 #include "pow_client.h"
 
 int main(int argc, char const *argv[]) {
-  if (argc < 2) {
-    std::cout << "Please provide player name." << std::endl;
-  }
-
   ProofOfWorkGameClient client("localhost:50051");
   client.Start();
 
   while (client.IsConnected()) {
     if (auto puzzle = client.PollNextPuzzle(1)) {
+      // Your code goes here
+      // Hint: you have to brute force the hash inputs
       std::cout << "Got new puzzle." << std::endl;
     }
   }
